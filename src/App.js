58bf8524467c94350634757ec6e468dd4c7bc8ff
maryanfawzy <maryanfawzy7@gmail.com>
2024-12-15 15:33:43 -0600

    import

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AllRoutes from './assests/routes/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes/>
+      
+    </div>
+  );
+}
+
+export default App;
