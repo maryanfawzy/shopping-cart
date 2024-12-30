ac63c0362b2349a4127065584a145e1f95ca7105
maryanfawzy <maryanfawzy7@gmail.com>
2024-12-30 17:09:23 -0600

    display

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -32,113 +32,80 @@
 //     )
 //   }
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import Rating from "./Rating"
+import Rating from "./Rating";
 
 export const ProductCard = ({ product }) => {
+  const {
+    mal_id: id,
 
-const {
+    title: name,
 
-mal_id: id,
+    synopsis: overview,
 
-title: name,
+    score: rating,
 
-synopsis: overview,
+    images: {
+      jpg: { image_url: poster },
+    },
 
-score: rating,
+    favorites,
+  } = product;
 
-images: { jpg: { image_url: poster } },
+  const price = "9.99";
 
-favorites,
+  // Consider a manga "best seller" if it has more than 50000 favorites
 
-} = product
+  const best_seller = favorites > 50000;
 
+  return (
+    <div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <Link to={`/products/${id}`} className="relative">
+        {best_seller && (
+          <span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
+            Best Seller
+          </span>
+        )}
 
-const price = "9.99"
+        <img
+          className="rounded-t-lg w-full h-64 object-cover"
+          src={poster}
+          alt={name}
+        />
+      </Link>
 
-// Consider a manga "best seller" if it has more than 50000 favorites
+      <div className="p-5">
+        <Link to={`/products/${id}`}>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {name}
+          </h5>
+        </Link>
 
-const best_seller = favorites > 50000
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {overview?.slice(0, 150)}
 
-return (
+          {overview?.length > 150 ? "..." : ""}
+        </p>
 
-<div className="m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex items-center my-2">
+          <Rating rating={rating ? rating / 2 : 0} />
+        </div>
 
-<Link to={`/products/${id}`} className="relative">
+        <p className="flex justify-between items-center">
+          <span className="text-2xl dark:text-gray-200">
+            <span>$</span>
 
-{best_seller && (
+            <span>{price}</span>
+          </span>
 
-<span className="absolute top-4 left-2 px-2 bg-orange-500 bg-opacity-90 text-white rounded">
+          <button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
+            Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
+          </button>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-Best Seller
-
-</span>
-
-)}
-
-<img
-
-className="rounded-t-lg w-full h-64 object-cover"
-
-src={poster}
-
-alt={name}
-
-/>
-
-</Link>
-
-<div className="p-5">
-
-<Link to={`/products/${id}`}>
-
-<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-
-{name}
-
-</h5>
-
-</Link>
-
-<p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-
-{overview?.slice(0, 150)}
-
-{overview?.length > 150 ? "..." : ""}
-
-</p>
-
-<div className="flex items-center my-2">
-
-<Rating rating={rating ? rating / 2 : 0} />
-
-</div>
-
-<p className="flex justify-between items-center">
-
-<span className="text-2xl dark:text-gray-200">
-
-<span>$</span>
-
-<span>{price}</span>
-
-</span>
-
-<button className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">
-
-Add To Cart <i className="ml-1 bi bi-plus-lg"></i>
-
-</button>
-
-</p>
-
-</div>
-
-</div>
-
-)
-
-}
-
-export default ProductCard
+export default ProductCard;
